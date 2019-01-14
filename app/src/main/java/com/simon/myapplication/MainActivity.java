e9fad4aaaa0d707543c4119ed0a8720b896ba9a5
SimonSM28 <gucci105>
2019-01-14 17:38:51 +0200

    Homework 3 14.11.19

--- app/src/main/java/com/simon/myapplication/MainActivity.java
+++ app/src/main/java/com/simon/myapplication/MainActivity.java
@@ -4,22 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView textView;
+    City moscow = new City("Moscow", -15, "South", 45, 490);
+    City madrid = new City("Madrid", 35, "North", 10, 126);
+    City porto = new City("Porto", 25, "West", 35, 340);
+    City newyork = new City("New York", -2, "North-West", 50, 980);
+    City paris = new City("Paris", 15, "South", 30, 540);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CityesOnClickListener();
-        //textView = findViewById(R.id.textView3);
-        //textView.animate().scaleX(2f).scaleY(2f).setDuration(6000);
-        //setContentView(new MyView(this));
         makeToast ("onCreate");
+        CheckBox windCheck = findViewById(R.id.wind_check);
+        TextView textView1 = findViewById(R.id.moscow_city);
+        TextView textView2 = findViewById(R.id.madrid_city);
+        TextView textView3 = findViewById(R.id.porto_city);
+        TextView textView4 = findViewById(R.id.new_york_city);
+        TextView textView5 = findViewById(R.id.paris_city);
+        textView1.setOnClickListener(this);
+        textView2.setOnClickListener(this);
+        textView3.setOnClickListener(this);
+        textView4.setOnClickListener(this);
+        textView5.setOnClickListener(this);
+        windCheck.setOnClickListener(this);
+
     }
 
     @Override
@@ -70,22 +85,41 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
     }
 
-    private void clickOnObject(TextView textView) {
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(intent);
-            }
-        };
-        textView.setOnClickListener(listener);
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(MainActivity.this, Main2Activity.class);
+        switch (v.getId()) {
+            case R.id.moscow_city:
+                putCityWeatherInfo(moscow.getName(), moscow.getTemperature(), moscow.getWind(),
+                        moscow.getHumidity(), moscow.getPreasure(), i);
+                break;
+            case R.id.madrid_city:
+                putCityWeatherInfo(madrid.getName(), madrid.getTemperature(), madrid.getWind(),
+                        madrid.getHumidity(), madrid.getPreasure(), i);
+                break;
+            case R.id.porto_city:
+                putCityWeatherInfo(porto.getName(), porto.getTemperature(), porto.getWind(),
+                        porto.getHumidity(), porto.getPreasure(), i);
+                break;
+            case R.id.new_york_city:
+                putCityWeatherInfo(newyork.getName(), newyork.getTemperature(), newyork.getWind(),
+                        newyork.getHumidity(), newyork.getPreasure(), i);
+                break;
+            case R.id.paris_city:
+                putCityWeatherInfo(paris.getName(), paris.getTemperature(), paris.getWind(),
+                        paris.getHumidity(), paris.getPreasure(), i);
+                break;
+        }
     }
-    private void CityesOnClickListener(){
-        clickOnObject(textView = findViewById(R.id.moscow_city));
-        clickOnObject(textView = findViewById(R.id.madrid_city));
-        clickOnObject(textView = findViewById(R.id.porto_city));
-        clickOnObject(textView = findViewById(R.id.new_york_city));
-        clickOnObject(textView = findViewById(R.id.paris_city));
+
+
+
+    public void putCityWeatherInfo (String name, int temp, String wind, int humid, int preasure, Intent i) {
+        i.putExtra("city", name);
+        i.putExtra("temperature", Integer.toString(temp));
+        i.putExtra("wind", wind);
+        i.putExtra("humidity", Integer.toString(humid));
+        i.putExtra("preasure", Integer.toString(preasure));
+        startActivity(i);
     }
 }
